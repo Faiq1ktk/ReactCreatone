52938481b6c064933a149e29b91ba8d70911f0f0
Faiq1ktk <145838796+Faiq1ktk@users.noreply.github.com>
2026-05-03 13:44:15 +0500

    Create Chai component, fix export, and correct JSX return structure in App

--- ReactCreatone/src/App.jsx
+++ ReactCreatone/src/App.jsx
@@ -1,7 +1,11 @@
+import Chai from "./chai";
 
 function App() {
   return (
-    <h1>ReactCreatOne</h1>
+    <>
+      <h1>ReactCreatOne</h1>
+      <Chai />
+    </>
   );
 }
 
---
+++ ReactCreatone/src/chai.jsx
@@ -0,0 +1,10 @@
+function Chai() {
+  return (
+    <>
+      <h1>ReactCreatOne from Chai.jsx</h1>
+      <h3>Chai , This is Ready!</h3>
+    </>
+  );
+}
+
+export default Chai;
